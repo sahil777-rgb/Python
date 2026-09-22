3d5cfd1ee3419f098e5fb66f722ec179faad8d25
sahil777-rgb <vmkbm7gfnf@privaterelay.appleid.com>
2026-09-22 11:16:45 +0530

    Initial commit

---
+++ 2.py
@@ -0,0 +1,18 @@
+d = {}
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+
+print(d)
+
+
+ 
---
+++ main.py
@@ -0,0 +1,3 @@
+s = {"madad": "help", "shukriya": "thank you", "khushi": "happiness", "dukh": "sadness", "pyaar": "love", "dosti": "friendship", "parivaar": "family", "jeevan": "life", "sapna": "dream", "safalta": "success"}
+word = input("Enter a word in Hindi: ")
+print(s[word])
