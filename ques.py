d982a29abccc37e03aca05f22e349c75bca4bfea
sahil777-rgb <vmkbm7gfnf@privaterelay.appleid.com>
2026-09-22 11:42:00 +0530

    problempractice

---
+++ ques.py
@@ -0,0 +1,16 @@
+d = {}
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+name = input("Enter first friend's name: ")
+lang = input("Enter first friend's favorite language: ")
+d.update({name: lang})
+
+print(d)
+
---
+++ quesp.py
@@ -0,0 +1,13 @@
+a = int(input("enter age of person: "))
+if (a>=18):
+    print("person is eligible for voting")
+    
+    
+    
+elif(a<0):
+    print("person is not born yet")
+else:
+    print("person is not eligible for voting")
+    
+print("end of prograam")            
+    
